18de0f5216d46f38e099aec6e99da6793c95e7cb
moi15moi <moi15moismokerlolilol@gmail.com>
2024-05-24 22:43:13 -0400

    [test.c] Add GDI font provider

--- test/test.c
+++ test/test.c
@@ -169,11 +169,12 @@ static void blend(image_t * frame, ASS_Image *img)
 }
 
 char *font_provider_labels[] = {
-    [ASS_FONTPROVIDER_NONE]       = "None",
-    [ASS_FONTPROVIDER_AUTODETECT] = "Autodetect",
-    [ASS_FONTPROVIDER_CORETEXT]   = "CoreText",
-    [ASS_FONTPROVIDER_FONTCONFIG] = "Fontconfig",
-    [ASS_FONTPROVIDER_DIRECTWRITE]= "DirectWrite",
+    [ASS_FONTPROVIDER_NONE]        = "None",
+    [ASS_FONTPROVIDER_AUTODETECT]  = "Autodetect",
+    [ASS_FONTPROVIDER_CORETEXT]    = "CoreText",
+    [ASS_FONTPROVIDER_FONTCONFIG]  = "Fontconfig",
+    [ASS_FONTPROVIDER_DIRECTWRITE] = "DirectWrite",
+    [ASS_FONTPROVIDER_GDI]         = "GDI",
 };
 
 static void print_font_providers(ASS_Library *ass_library)
